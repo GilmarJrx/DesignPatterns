ef68b90b6e73ca7fbe84c0f48e6bcf30fabba492
Gilmar Oliveira <72040832+GilmarJrx@users.noreply.github.com>
2025-02-22 15:40:42 -0300

    SOLI[D] - Dependency Inversion Principle

--- Program.cs
+++ Program.cs
@@ -2,8 +2,10 @@
 using DesignPatterns.SOLID.OCP;
 using DesignPatterns.SOLID.LSP;
 using DesignPatterns.SOLID.ISP;
+using DesignPatterns.SOLID.DIP;
 
-//SingleResponsibilityPrinciple.RunDemo();
-//OpenClosedPrinciple.RunDemo();
-//LiskovSubstitutionPrinciple.RunDemo();
+SingleResponsibilityPrinciple.RunDemo();
+OpenClosedPrinciple.RunDemo();
+LiskovSubstitutionPrinciple.RunDemo();
 InterfaceSegregationPrinciple.RunDemo();
+DependencyInversionPrinciple.RunDemo();
